82fa11635afa1fb1bf787eac5c72181e924b1fe9
Kiet Nguyen <kietnguyen@nexondv.com>
2024-02-20 18:00:29 +0700

    add card

--- src/views/sections/Intro.vue
+++ src/views/sections/Intro.vue
@@ -11,13 +11,37 @@ defineProps<{
     scroll-snap-stop: always;
     height: calc(100vh - 5rem * 2);
     padding: 5rem;
-    background-color: #edeae5;
+    background-color: #e3e2df;
+    background-image: repeating-linear-gradient(
+            45deg,
+            #e3afbc 25%,
+            transparent 25%,
+            transparent 75%,
+            #e3afbc 75%,
+            #e3afbc
+        ),
+        repeating-linear-gradient(
+            45deg,
+            #e3afbc 25%,
+            #e5e5f7 25%,
+            #e5e5f7 75%,
+            #e3afbc 75%,
+            #e3afbc
+        );
+    background-position:
+        0 0,
+        2rem 2rem;
+    background-size: 4rem 4rem;
 }
 
 .intro-content {
     .intro-content__title {
         text-align: center;
         margin-bottom: 2rem;
+        padding: 4rem 8rem;
+        background-color: #feffff;
+        border-radius: 50%;
+        border: 2px solid gray;
     }
 }
 
@@ -26,10 +50,26 @@ defineProps<{
     display: flex;
     flex-direction: row;
     justify-content: center;
-    gap: 2rem;
-    .intro-content-images__image {
-        height: 40rem;
-        width: fit-content;
+    gap: 4rem;
+}
+
+.image-card {
+    cursor: pointer;
+    height: 35rem;
+    width: fit-content;
+    padding: 0.5rem;
+    border-radius: 1rem;
+    outline-style: dashed;
+    outline-color: #5d001e;
+    overflow: hidden;
+
+    .image-card__image {
+        transition: all 0.5s ease-in-out;
+        border-radius: 1rem;
+
+        &:hover {
+            transform: scale(1.5, 1.5);
+        }
     }
 }
 </style>
@@ -37,25 +77,32 @@ defineProps<{
 <template>
     <div v-bind:id="sectionId" class="intro-section">
         <div class="intro-content">
-            <h1 class="intro-content__title">Introduce Section</h1>
-            <h6>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </h6>
+            <div class="intro-content__title">
+                <h1>Introduce Section</h1>
+                <h6>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </h6>
+            </div>
             <div class="intro-content-images">
-                <img
-                    class="intro-content-images__image"
-                    src="/assets/images/image-1.jpg"
-                />
-                <img
-                    class="intro-content-images__image"
-                    src="/assets/images/image-2.jpg"
-                />
+                <div class="image-card">
+                    <img
+                        class="image-card__image"
+                        src="/assets/images/image-1.jpg"
+                    />
+                </div>
+                <div class="image-card">
+                    <img
+                        class="image-card__image"
+                        src="/assets/images/image-2.jpg"
+                    />
+                </div>
             </div>
         </div>
     </div>
